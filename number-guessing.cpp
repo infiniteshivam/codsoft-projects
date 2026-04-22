44480b6bc5c1f2bbea79a84d1c6fa6176cd7bc62
Shivam krishna <ishivamkrishna@gmail.com>
2026-04-23 01:34:50 +0530

    added a easy level

--- number-guessing.cpp
+++ number-guessing.cpp
@@ -5,22 +5,48 @@
 using namespace std;
 
 int main() {
-    random_device seed;
-    mt19937 engine(seed());
-    uniform_int_distribution<> range(1, 6); // Ludo ka Dice
+    // random_device seed;
+    // mt19937 engine(seed());
+    // uniform_int_distribution<> range(1, 6); // Ludo ka Dice
     string inp;
     cout<<"WELCOME TO THE NUMBER GUESSING GAME\n";
     cout<<"INSTRUCTION: YOU HAVE TO GUESS THE NUMBER \n";
-    cout<<"CHOOSE THE DIFFICULTY LEVEL: \n EASY \n MEDIUM \n HARD";
-    if(inp == "EASY"){
-
+    cout<<"CHOOSE THE DIFFICULTY LEVEL: \n EASY \n MEDIUM \n HARD\n\n ->";
+    cin>>inp;
+    transform(inp.begin(),inp.end(),inp.begin(),::tolower);
+    cout<<inp;
+    if(inp == "easy"){
+        cout<<"\nYOU CHOOSED (LEVEL > EASY)\n";
+        cout<<"LETS BEGIN!!";
+        int x;
+        int count=0;
+        random_device seed;
+        mt19937 engine(seed());
+        uniform_int_distribution<> range(1, 20);
+        cout<<"CHOOSE A NUMBER BETWEEN(0-20): \n";
+        while(x!=range(engine)){
+        cin>>x;  
+        if(x<range(engine)){
+            cout<<"BIT MORE!";
+        }
+        else if(x>range(engine)){
+            cout<<"BIT LESS!";
+        } 
+        else{
+            cout<<"YE GUESSED IT RIGHT!!!";
+            break;
+            
+        }count++;
+        
     }
+    cout<<"ATTEMPTS : "<<count;
+}
     
 
-    for(int i = 0; i < 5; i++) {
-        // Sirf distr(gen) baar-baar bulao
-        cout << "Dice Roll " << i+1 << ": " << range(engine) << endl;
-    }
+    // for(int i = 0; i < 5; i++) {
+    //     // Sirf distr(gen) baar-baar bulao
+    //     cout << "Dice Roll " << i+1 << ": " << range(engine) << endl;
+    // }
     return 0;
 }
 
