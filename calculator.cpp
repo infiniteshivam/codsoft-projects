5d2ed3cbf2b121ca1ed1709e0bd26873bc48d221
Shivam krishna <ishivamkrishna@gmail.com>
2026-04-27 22:05:04 +0530

    code all done
    it is basic used switch cases to filter through the input given by the user and then performing the necessary actions possible

--- calculator.cpp
+++ calculator.cpp
@@ -1,10 +1,43 @@
 //simple calculator
-//description : basically gets 
+//description : basically gets input from the user and performs the task that user wants.
 
 #include<iostream>
 using namespace std;
+int x,y;
+char op;
 int main(){
-
+    cout<<"******************************************"<<endl;
+    cout<<"                CALCULATOR"<<endl;
+    cout<<"******************************************"<<endl;
+    cout<<"INPUT A NUMBER: "<<endl;
+    cin>>x;
+    cout<<"FUNCTIONS YOU WANT TO PERFORM: \n + | - | * | / "<<endl;
+    cin>>op;
+    cout<<"ENTER OTHER NUMBER: "<<endl;
+    cin>>y;
+    switch (op)
+    {
+    case '+':
+       cout<<x+y;
+        break;
+    case '-':
+       cout<<x-y;
+        break;
+    case '*':
+       cout<<x*y;
+        break;
+    case '/':
+        if(y!= 0){
+            cout<<x/y;
+        }
+        else{
+            cout<<"INVALID: ZERO DIVISON ERROR";
+        }
+        break;
+    default:
+        cout<<"INVALID OPERATOR \n KINDLY CHOOSE BETWEEN + | - | * | /";
+            break;
+        }
 
     return 0;
 }
---
+++ tictactoe.cpp
@@ -0,0 +1,10 @@
+//tic tac toe
+//this is basically a tic tac toe game based 
+
+#include <iostream>
+using namespace std;
+int main(){
+
+
+    return 0;
+}
